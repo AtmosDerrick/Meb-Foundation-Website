49bb4a90af519848913f38e5e3f8262a6b6e5a79
atmos <atmosderrick@gmail.com>
2023-01-13 18:43:42 +0000

    Project to deply

---
+++ js/all.js
@@ -0,0 +1,64 @@
+
+
+
+const menubar = document.getElementById('menuResponsive').addEventListener('click', responsiveNavbar);
+function responsiveNavbar() {
+  var topnav = document.getElementById("showtopnav");
+  topnav.classList.toggle("responsive");
+
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+   document.addEventListener("scroll", function() {
+    const top = document.getElementById("top");
+    const whitetext = document.getElementsByClassName('whitetext');
+    const whitetextdonate = document.getElementById('whitetextdonation');
+
+
+    
+    if (window.pageYOffset > 300) {
+      top.classList.add("bg-gray-900");
+
+
+
+      whitetextdonate.classList.add("donatewhite");
+
+      whitetextdonate.classList.add("text-yellow-500");
+
+
+      
+
+      for(let i =0; i < whitetext.length; i++){
+        whitetext[i].classList.add("text-white")
+        
+
+      }
+      
+    }
+
+    else if(window.pageYOffset < 300) {
+      top.classList.remove("bg-gray-900");
+      whitetextdonate.classList.remove("donatewhite");
+      whitetextdonate.classList.remove("text-yellow-500");
+
+
+
+
+      for(let i =0; i < whitetext.length; i++){
+        whitetext[i].classList.remove("text-white")
+
+
+      }
+
+    }
+  });
